a51e3230a365ca7c11e523690d8907507a618420
JsLealM <johanleal2002@gmail.com>
2025-01-28 01:12:57 -0500

    App en Desarrollo

--- app.js
+++ app.js
@@ -5,11 +5,20 @@ function agregarAmigo (){
  let amigo = document.getElementById('amigo').value;
  if (amigo === ''){
   alert('Por favor, inserte un nombre');
+  mostrarListaAmigos ();
  }else{
   amigos.push(amigo);
   document.getElementById('amigo').value = ''
+  mostrarListaAmigos ();
   console.log(amigos);
  }
 }
 
+function mostrarListaAmigos (){
+  let lista = document.getElementById('listaAmigos');
+  lista.innerHTML = '';
+  for (let i = 0; i < amigos.length; i++) {
+    lista.innerHTML = amigos[i];
+  }
+}
 console.log(amigos);
